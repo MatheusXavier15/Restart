c250c8850f415f3a4c15030f1a34e80739d4f33b
Matheus Xavier <matheus.oliveira@rhitmo.com.br>
2023-07-26 21:31:09 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Matheus Xavier on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
